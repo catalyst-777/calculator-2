01c66713ab1079248b543db5f7aecd1733432ce7
Courtney Doss <777.catalyst@gmail.com>
2021-09-29 15:41:56 -0500

    Added all functionality for Cal2

--- calculator.py
+++ calculator.py
@@ -3,5 +3,32 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-
-# Replace this with your code
+while True:
+    user_input = input("Enter your equation > ").rstrip()
+    tokens = user_input.split(" ")
+    for i in range(1,len(tokens)):
+        print(tokens[i])
+        tokens[i] = float(tokens[i])
+    for token in tokens:
+        print(f"in for loop {type(token)}")
+    if tokens[0] == "q":
+        break
+    elif tokens[0] == '+':
+        print(add(tokens[1],tokens[2]))
+    elif tokens[0] == '-':
+        print(subtract(tokens[1],tokens[2]))
+    elif tokens[0] == '*':
+        print(multiply(tokens[1],tokens[2]))
+    elif tokens[0] == '/':
+        print(divide(tokens[1],tokens[2]))
+    elif tokens[0] == 'square':
+        print(square(tokens[1]))
+    elif tokens[0] == 'cube':
+        print(cube(tokens[1]))
+    elif tokens[0] == 'pow':
+        print(power(tokens[1],tokens[2]))
+    elif tokens[0] == 'mod':
+        print(mod(tokens[1],tokens[2]))
+    else:
+        print("in else")
+  
